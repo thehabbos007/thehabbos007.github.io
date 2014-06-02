23fc4db8140d7001bd5103a3e6adaf03724183e7
Ahmad S. Atta <thehabbos007@gmail.com>
2014-06-02 18:47:40 +0200

    Numeric only
    Numeric only

--- scripts/script.js
+++ scripts/script.js
@@ -136,4 +136,13 @@ function chk(fld)
     }
 }
 
-	
+function validate(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
